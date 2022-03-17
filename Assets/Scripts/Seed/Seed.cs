1d4bdbfbd7e0ba1cd7a3d761a2b5a0297df57786
Jose1928Death <jose1928death@gmail.com>
2022-03-17 21:47:51 +0100

    semilla
    semilla

---
+++ Assets/Scripts/Seed/Seed.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed : MonoBehaviour
+{
+    public GameObject tree;
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("germinate", time);
+    }
+    private void germinate()
+    {
+        if (transform.position.y < 0.5f)
+        {
+            Vector3 pos = new Vector3(transform.position.x, 0.5f, transform.position.z);
+            Instantiate(tree, pos, Quaternion.identity);
+        }
+        Destroy(this.gameObject);
+    }
+}
